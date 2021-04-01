831e4587b9da749b66e9ab137dab2fc816bf7cfd
Raghav <raghavsrm@gmail.com>
2021-04-01 00:25:54 -0500

    day 2,3 counter, FindAllEvenOddIndexedNumbersInArray fixes

--- src/com/raghav/javabasics/Counter.java
+++ src/com/raghav/javabasics/Counter.java
@@ -5,56 +5,96 @@ public class Counter {
 
     public static void main(String[] args) {
         Counter cnt = new Counter();
-        cnt.wordsCharLines("hi my name is chaitu");
         cnt.wordsCharLines("");
-        cnt.wordsCharLines(null);
-        cnt.wordsCharLines("this,is.also.a.sentence");
-        cnt.wordsCharLines("\n !@#$% !@#$%\n");
-        cnt.wordsCharLines("Change an Array\n" +
-                "new\n" +
-                "out\n" +
-                "Element");
-    }
+//        cnt.wordsCharLines("");
+//        cnt.wordsCharLines(null);
+//        cnt.wordsCharLines("\n !@#$% !@#$%\n");
+//        cnt.wordsCharLines("Change an Array\n" +
+//                "new\n" +
+//                "out\n" +
+//                "Element\n");
+        cnt.wordsCharLines("raghav!!4334 ram");
 
-    public int words(String paragraph){
-        int wordCount=1;
-        for(int i=0;i<paragraph.length();i++){
-            char c = paragraph.charAt(i);
-            if(c == ' ' || c == '\n'|| c == ','|| c == '.'){
-                wordCount = wordCount+1;
+    }
+    public boolean isLetter(char c){
+        int asciiValue= (int)c;
+        if((asciiValue >= 97 && asciiValue <= 122) || (asciiValue >= 65 && asciiValue <=90)){
+            return true;
+        }
+        return false;
+    }
+// 2 pointer technique
+    public int words1(String paragraph) {
+        int wordCount =0;
+        if(isLetter(paragraph.charAt(0))){
+            wordCount+=1;
+        }
+        for (int i=1; i<paragraph.length();i++){
+            if(paragraph.charAt(i-1)==' ' && isLetter(paragraph.charAt(i))){
+                wordCount+=1;
             }
         }
+
         return wordCount;
     }
-    public int characters(String paragraph){
-        int charCount=0;
-        for(int i=0;i<paragraph.length();i++){
-
-            char c = paragraph.charAt(i);
-            charCount = charCount+1;
+        private void wordsCharLines(String paragraph) {
+        if (paragraph == null || paragraph.length() == 0) {
+            System.out.println("String is empty/null");
+        } else {
+            int[] result = new int[3];
+            result[0] = words1(paragraph);
+            result[1] = characters(paragraph);
+            result[2] = lines(paragraph);
+            for (int i = 0; i < result.length; i++)
+                System.out.println(result[i]);
         }
-        return charCount;
     }
-    public int lines(String paragraph){
-        int lineCount=1;
-        for(int i=0;i<paragraph.length();i++){
+
+    private int lines(String paragraph) {
+        int lineCount = 1;
+        for (int i = 0; i < paragraph.length() - 2; i++) {
             char c = paragraph.charAt(i);
-            if(c == '\n'){
-                lineCount = lineCount+1;
+            if (c == '\n') {
+                lineCount = lineCount + 1;
             }
         }
         return lineCount;
     }
-    public void wordsCharLines(String paragraph){
-        if(paragraph==null || paragraph.length()==0){
-            System.out.println("String is empty/null");
+
+    private int characters(String paragraph) {
+        int charCount = 0;
+        for (int i = 0; i < paragraph.length(); i++) {
+
+            char c = paragraph.charAt(i);
+            charCount = charCount + 1;
+        }
+        return charCount;
+    }
+
+    public int words(String paragraph) {
+        int length= paragraph.length();
+        int wordCount = 0;
+        if(length==1 ) {
+            if(paragraph.charAt(0) == ' ' || paragraph.charAt(0) == '\n' || paragraph.charAt(0) == ',' || paragraph.charAt(0) == '.' || paragraph.charAt(0) =='!' )
+                return 0;
         }
         else {
-            int[] result = new int[3];
-            result[0] = words(paragraph);
-            result[1] = characters(paragraph);
-            result[2] = lines(paragraph);
-            for (int i = 0; i < result.length; i++)
-                System.out.println(result[i]);        }
+            for (int i = 0; i < paragraph.length(); i++) {
+
+                char c = paragraph.charAt(i);
+                if (i >= 1) {
+                    char d = paragraph.charAt(i - 1);
+                    if (c == ' ' || c == '\n' || c == ',' || c == '.' || c == '!') {
+                        if (d == ' ' || d == '\n' || d == ',' || d == '.' || d == '!') {
+                            wordCount = wordCount + 0;
+                        }
+                        else {
+                            wordCount = wordCount + 1;
+                        }
+                    }
+                }
+            }
+        }
+        return wordCount;
     }
 }
--- src/com/raghav/javabasics/FindAllEvenOddIndexedNumbersInArray.java
+++ src/com/raghav/javabasics/FindAllEvenOddIndexedNumbersInArray.java
@@ -6,20 +6,47 @@ public class FindAllEvenOddIndexedNumbersInArray {
     public static void main(String[] args) {
         FindAllEvenOddIndexedNumbersInArray result = new FindAllEvenOddIndexedNumbersInArray();
         result.countIndices(new int[]{5,-5,6,2,1,99,23});
-        }
+        result.countIndices(new int[]{});
+        result.countIndices(new int[]{1,2,3,4,5,6});
+        result.countIndices(new int[]{1});
+        result.countIndices(new int[]{1,2});
+    }
 
     public void countIndices(int[] myArr){
+        if (myArr == null || myArr.length == 0) {
+            System.out.println("Array is Empty/Null");
+            return;
+        }
         int evenCount=0;
         int oddCount =0;
+        int count1=0;
+        int count2=0;
+        int[] evenCountArr = new int[myArr.length/2+1 ];
+        int[] oddCountArr = new int[myArr.length/2 ];
+
         for( int i=0; i< myArr.length; i++){
             if(i == 0 || i % 2 ==0){
                 evenCount = evenCount + 1;
+                evenCountArr[count1]=myArr[i];
+                count1+=1;
             }
             else{
                 oddCount = oddCount + 1;
+                oddCountArr[count2]=myArr[i];
+                count2+=1;
             }
         }
-        System.out.println("Count of Even Indexes : "+evenCount+" Count of Odd Indexes : "+oddCount);
-    }
+        for(int i=0; i<count1;i++){
+            if(i==0){
+                System.out.print("Even indices list: ");
+            }
+            System.out.print(evenCountArr[i]+" ");
+        }
+        for(int i=0; i<count2;i++){
+            if(i==0){
+                System.out.print("   Odd indices list: ");
+            }
+            System.out.print(oddCountArr[i]+" ");
+        }    }
 }
 
